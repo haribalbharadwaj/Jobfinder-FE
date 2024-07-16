84e3119057c65b58feefcbd59e65d1b683a20f3f
BHARADWAJ HARIBAL <haribalbharadwaj@gmail.com>
2024-07-16 08:41:34 +0530

    hello

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
